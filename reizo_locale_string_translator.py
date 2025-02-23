6e54571625165074f51086f47eeab43a4bf912a6
[Reizo] <59807064+Reizo0337@users.noreply.github.com>
2025-02-23 18:18:56 +0100

    Update reizo_locale_string_translator.py
    100% functionally.

--- reizo_locale_string_translator.py
+++ reizo_locale_string_translator.py
@@ -1,4 +1,8 @@
 #!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+## we can also make it async and send the translation asynchronously but they will not come ordered.. And that's not good.
+
 import asyncio
 from googletrans import Translator
 
@@ -14,25 +18,36 @@ valid_languages = ['en', 'es', 'fr', 'de', 'it', 'pt', 'ko', 'ja', 'zh-cn', 'zh-
 if src_lang not in valid_languages or dest_lang not in valid_languages:
     print("Invalid language code entered. Please use valid language codes (e.g., 'en', 'es', 'fr').")
     exit()
+    
+def clean_translation(text):
+    text = text.replace("„", "").replace("”", "").strip()
+    return text
+
+def is_an_empty_line(line):
+    return not line.strip()
 
 async def translate_text():
     with open(input_file, "r", encoding="utf-8") as infile, open(f"{output_file}_{dest_lang}.txt", "w", encoding="utf-8") as outfile:
         lines = infile.readlines()
 
+        tasks = []
         for i in range(0, len(lines), 2):
             source_text = lines[i].strip()
 
-            if i + 1 < len(lines):
-                translated_text = lines[i + 1].strip()
-            else:
-                translated_text = ""
+            if is_an_empty_line(source_text):
+                continue
+            
+            translation = await translator.translate(source_text, src=src_lang, dest=dest_lang)
+            translated_text = translation.text
+            
+			#clean text.
+            translated_text = clean_translation(translated_text)
 
-            if not translated_text:
-                translation = await translator.translate(source_text, src=src_lang, dest=dest_lang)
-                translated_text = translation.text
+            outfile.write(f'{source_text}\n')
+            outfile.write(f'"{translated_text}"\n')
 
-            outfile.write(f'"{source_text}" -> "{translated_text}"\n')
-            print(f'"{source_text}" -> "{translated_text}"')
+            print(f'"{source_text}"\n')
+            print(f'"{translated_text}"\n')
 
 asyncio.run(translate_text())
 
